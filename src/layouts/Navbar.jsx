e610b080ee4c84b2fec0df0da6a31ce01d372f23
repoNelly <nellytachirinos@gmail.com>
2023-06-04 10:40:32 -0300

    Hice cambios en package y .css

--- src/layouts/Navbar.jsx
+++ src/layouts/Navbar.jsx
@@ -7,16 +7,14 @@ const NavBarExample = () => {
         <>  
     <Navbar className="navBg" collapseOnSelect expand="lg" bg="gray" variant="gray">
         <Container id="container">
-            
-            
             <Nav.Link id="nav" as={Link} to="/" >
                 <img src="./img/logo.png" width='230'/>
             </Nav.Link>
             <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-                <Nav.Link id="lista" as={Link} to="/" >Inicio</Nav.Link>
-                <Nav.Link id="lista" as={Link} to="/nosotros">Nosotros</Nav.Link>
-                <Nav.Link id="lista" as={Link} to="/contacto">Contacto</Nav.Link>                
+                <Nav.Link id="lista" as={Link} to="/" ><span><span>Inicio</span></span></Nav.Link>
+                <Nav.Link id="lista" as={Link} to="/nosotros"><span><span>Nosotros</span></span></Nav.Link>
+                <Nav.Link id="lista" as={Link} to="/contacto"><span><span>Contacto</span></span></Nav.Link>                
             </Nav>
             </Navbar.Collapse>
         </Container>

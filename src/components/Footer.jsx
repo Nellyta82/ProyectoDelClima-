540b88b96b301dd14065e6f1f32cd16438c7c83c
repoNelly <nellytachirinos@gmail.com>
2023-06-14 15:03:27 -0300

    haciendo responsivo hasta tamaño tablet

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,42 +7,33 @@ const Footer = () => {
     
     <div id="container3"> 
 
-    <footer classpie="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+     <footer classpie="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
         <div classpie="col-md-4 d-flex align-items-center">
             <a href="/" classpie="mb-3 me-2 mb-md-0 text-body-secondary text-decoration-none lh-1">
             
                 <svg classpie="bi" width="30" height="24"><use xlink:href="#bootstrap"/></svg>
             </a>
             <span classpie="mb-3 mb-md-0 text-body-secondary">
-              
-              
                 <h4 id='titulo-footer'>Un salto al Clima</h4>
-                </span>
+            </span>
         </div>
         
         <div className='col-sm-2 col-lg-4 col-xs-12 footer-text-contac text-center' align='text-center'>
-        <h6>
-            <span className='font-footer'>  
-            <h3>Contacto</h3>
-            </span>
-        </h6>
-        <p>Dirección: Av. Rivadavia 5200 CABA-Argentina</p>
-        
-        <p>unsaltoalclima@gmail.com</p>
-        
-        
-         
-    </div>
-    <div id='container2'>
-
-        <ul classpie="nav col-md-4 list-unstyled d-flex">
-            
+            <div id="list-footer">
+                <span className='font-footer'>  
+                <h6>Contacto</h6>
+                </span>
+                <h6>Dirección: Av. Rivadavia 5200 CABA-Argentina</h6>
+                <h6>unsaltoalclima@gmail.com</h6>    
+            </div>
+        </div>
+        <div id='container2'>
+         <ul classpie="nav col-md-4 list-unstyled d-flex">
             <button><img src="./img/Twitter.jpg" alt="instagram" width='50px' height='50px'/></button>
             <button><img src="./img/Instagram (2).jpg" alt="instagram" width='50px' height='50px'/></button>
-            <button><img src="./img/Facebook.jpg" alt="instagram" width='50px' height='50px'/></button>
-           
-        </ul>
- </div>
+            <button><img src="./img/Facebook.jpg" alt="instagram" width='50px' height='50px'/></button>  
+         </ul>
+        </div>
 
     </footer>
 </div>

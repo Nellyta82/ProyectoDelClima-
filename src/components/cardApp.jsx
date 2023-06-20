e02906ec6d7149ee14e3b8e64f11f21aed313972
repoNelly <nellytachirinos@gmail.com>
2023-06-20 10:08:30 -0300

    Arreglo del dbjson

--- src/components/cardApp.jsx
+++ src/components/cardApp.jsx
@@ -5,7 +5,7 @@ import { LoadingButton } from "@mui/lab";
 import { Box, Container, TextField, Typography } from "@mui/material";
 import { useState } from "react";
 
-const API_WEATHER = `http://api.weatherapi.com/v1/current.json?key=${import.meta.env.VITE_API_KEY}&lang=es&q=`;
+const API_WEATHER = `https://api.weatherapi.com/v1/current.json?key=${import.meta.env.VITE_API_KEY}&lang=es&q=`;
 
 export function Card() {
   const [city, setCity] = useState("");

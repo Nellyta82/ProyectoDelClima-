242f606627d975250d19864a4d7b1f27de2971bd
Yoselyn Diaz <yoselynalejandra@gmail.com>
2023-06-22 15:48:11 -0300

    arreglo boton

--- src/components/Principal.jsx
+++ src/components/Principal.jsx
@@ -1,4 +1,5 @@
     import { Button } from "react-bootstrap"
+    import { Link } from "react-router-dom";
 
 
 const Principal = () => {
@@ -10,7 +11,7 @@ const Principal = () => {
         <p id="principal-text"> Durante este Invierno hay una mayor probabilidad de que se registren, en promedio, temperaturas superiores a lo normales. En el extremo norte del país y el centro y norte de Patagonia se favorecen valores dentro del promedio para la época, mientras que en Tierra del Fuego y el sur de Santa Cruz incluso pueden estar por debajo de la media.
         Los cambios de temperaturas son un caos por eso se recomienda no salir de casa sin ver el pronóstico del Clima.</p>
         <br />
-        <button id="punto"><a href="/Inicio">Consulta el Clima</a></button>
+        <button id="punto"><Link to="/Inicio">Consulta el Clima</Link></button>
         <br /> 
         <br />
     </div>  
